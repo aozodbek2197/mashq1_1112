032be1de53134e6bcad9bedbf11a9b2175fb7eb6
aozodbek2197 <aozodbek2197@gmail.com>
2025-12-11 16:23:39 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+# # 1-mashq
+# summa = lambda x, y=5: x * y
+# result = summa(7)
+# print(result)
+# # 2-mashq
+# kopaytir = lambda a, b: a * b
+# print(kopaytir(4, 5))
+# # 3-mashq
+# toq_juft = lambda x: "juft" if x % 2 == 0 else "toq"
+
+# print(toq_juft(8))
+# print(toq_juft(7))
+# # 4-mashq
+# katta_kichik = lambda x, y: "kichik" if x < y else "katta"
+# print(katta_kichik(5, 3))
+# print(katta_kichik(3, 5))
+# # 5-mashq
+# uzunlik = lambda text: "True" if len(text) > 7 else "False"
+
+# print(uzunlik("Hello World!"))
+# print(uzunlik("hello"))
